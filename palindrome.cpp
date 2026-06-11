70657ad7d19abfcadc25015af0ff75ba9b974571
abir781 <hasan.abir3176@gmail.com>
2026-06-11 06:34:41 +0600

    kkksfd

---
+++ palindrome.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+ 
+
+   
+
+
+        int n;
+  cin>>n;
+
+  int* arr = new int[n];
+
+
+
+     
+    
+
+
+
+
+    }
+      
+
+
+
+ 
+
+
+
+
+
+
+
+
+    return 0;
+}
+
